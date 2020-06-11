956d0d2b353973809de9e576d3978adcc3229d55
moT01 <20648924+moT01@users.noreply.github.com>
2020-06-11 11:58:23 -0500

    30.1

--- coderoad/test/f-t.test.js
+++ coderoad/test/f-t.test.js
@@ -47,3 +47,33 @@ describe("index.html", () => {
 		assert(/<!doctype html>/i.test(indexFile));
 	});
 });
+
+const removeWhiteSpace = (str = '') => {
+  return str.replace(/\s/g, '');
+};
+
+const getPsqlLogFile = async (dir = process.cwd()) => {
+	const pathToPsqlLogFile = path.join(dir, '../../../../var/log/postgresql', 'postgresql-12-main.log');
+	const psqlLogFile = await readFile(pathToPsqlLogFile, 'utf8');
+
+  if (!psqlLogFile) {
+    throw new Error(`Could not find ${pathToPsqlLogFile}`);
+  }
+	return psqlLogFile;
+}
+
+describe("database", () => {
+  let logFile;
+  before(async () => {
+    logFile = await getPsqlLogFile();
+  });
+
+	it('should have a log', () => {
+    console.log('logFile')
+    logFile = logFile.split('LOG:  statement: ');
+    const lastLog = removeWhiteSpace(logFile[logFile.length-1]);
+    console.log(lastLog);
+    const re = /SELECTd\.datnameas"Name",pg_catalog\.pg_get_userbyid\(d\.datdba\)as"Owner",pg_catalog\.pg_encoding_to_char\(d\.encoding\)as"Encoding",d\.datcollateas"Collate",d\.datctypeas"Ctype",pg_catalog\.array_to_string\(d\.datacl,E'\\n'\)AS"Accessprivileges"FROMpg_catalog\.pg_databasedORDERBY1;$/;
+		assert(re.test(lastLog));
+	});
+});
